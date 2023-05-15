ad296009bd3044dc1ac4faf160e9961829e9d3b7
aligorbor <aligorbor@list.ru>
2023-05-15 12:09:55 +0300

    Added custom SpotLightImageView

---
+++ app/src/main/java/com/example/android/customfancontroller/spotlight/SpotLightActivity.kt
@@ -0,0 +1,26 @@
+package com.example.android.customfancontroller.spotlight
+
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import com.example.android.customfancontroller.R
+
+class SpotLightActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spot_light)
+        val dialog = createInstructionsDialog()
+        dialog.show()
+    }
+
+    private fun createInstructionsDialog(): Dialog {
+        val builder = AlertDialog.Builder(this)
+        builder.setIcon(R.drawable.android)
+            .setTitle(R.string.instructions_title)
+            .setMessage(R.string.instructions)
+            .setPositiveButtonIcon(ContextCompat.getDrawable(this, android.R.drawable.ic_media_play))
+        return builder.create()
+    }
+}
